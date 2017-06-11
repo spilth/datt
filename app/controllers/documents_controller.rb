8ad433964e4fb93cee903f823eed1a0b7a017ec0
Brian Kelly <polymonic@gmail.com>
2017-06-11 17:11:45 -0400

    Order documents by Title on Document page

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -4,7 +4,7 @@ class DocumentsController < ApplicationController
   # GET /documents
   # GET /documents.json
   def index
-    @documents = Document.all
+    @documents = Document.all.order(:title)
   end
 
   # GET /documents/1
